186f03d6d0f057eac8248bd1dd78e4832ab1f6a8
addthoriq <alvarro.thoriq@gmail.com>
2021-08-27 14:33:37 +0800

    make button and toast

--- app/src/main/java/com/example/kotlindroid/MainActivity.kt
+++ app/src/main/java/com/example/kotlindroid/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.kotlindroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buton = findViewById(R.id.tombul) as Button
+
+        buton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Berhasil", Toast.LENGTH_LONG).show()
+        }
     }
 }
